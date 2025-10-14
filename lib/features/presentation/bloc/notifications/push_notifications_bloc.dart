0cc47ee9b78281691849c0ee965dab898b719b61
Mohammed Raza <raza.m@incitesol.com>
2025-10-14 18:19:36 +0530

    Moved service account json file to assets folder

--- lib/features/presentation/bloc/notifications/push_notifications_bloc.dart
+++ lib/features/presentation/bloc/notifications/push_notifications_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:sample_project/core/error/exception_handler.dart';
@@ -67,10 +69,12 @@ class PushNotificationsBloc
         credentials = FirebasePushNotifications.authClient;
       }
 
+      if (credentials == null) return;
+
       var headers = {
         'Content-Type': 'application/json; charset=UTF-8',
         'Authorization':
-            '${credentials?.credentials.accessToken.type} ${credentials?.credentials.accessToken.data}'
+            '${credentials.credentials.accessToken.type} ${credentials.credentials.accessToken.data}'
       };
 
       var result = await _repository.fcmPushNotification(body, headers);
@@ -90,33 +94,27 @@ class PushNotificationsBloc
   }
 
   /// Obtain Authenticated client using private key from firebase admin sdk
-  Future<AuthClient> obtainAuthenticatedClient() async {
+  Future<AuthClient?> obtainAuthenticatedClient() async {
     /// https://console.firebase.google.com/project/farmers-market-c591f/settings/serviceaccounts/adminsdk
     /// Generate New private key
-    final accountCredentials = ServiceAccountCredentials.fromJson({
-      "type": "service_account",
-      "project_id": "farmers-market-c591f",
-      "private_key_id": "f2619fc8927dbce53188758a72debb5febbef132",
-      "private_key":
-          "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDINBJ408hIiwsg\nxNwZ3Mz3iMbflBjDjric/uHmS2HSsPDoBHe0aKNG8jw/oft67zF1fboO1Jin6WS+\nuQXNnuPV0UZCNZ6ZaIa87XIN4w4DkNj2TprCPbUWQuxScJmtdSJtG2CvSjp9ynlN\nnfPcmNPcsVBmT1thwKGv4yMkAMeF08+LlT5q9f7R8EbUBTGwLsT2MgiPYeVVKrZp\n5TQ6oVR0B6yDSWnXpiGI3XFKgLuyJUtEdQBJFnXp3Zm8hl9qcJDe8xz/whz3bl6K\n+iVVPjkJs78oXMsk1TDKH9a262VVti+25gi+at7pqcjgxncumqtXlgA52xZ2QG1x\nbi0GstrtAgMBAAECggEARG/gfKNQzNOTG0GQR9r9ygRcj2Mtz/PC0hAjIrlUGFTa\nFUYpsWcvoXn+9HWn5M9NZe9zocw03vlesf/HpxGQqq/eR5XSwHhFzuGQYoTibn+e\nWxxltM6pT5HS/R92uR/Yg9mfBGzob8W+D7VV/mvTyJZoeIDaKSFTZC/N/GmzyuAQ\nw9JM9SMZC6oL3xURL6dZg89/D/TfWJnDobOySj7XziyXswP2R011IrjOUWEiwUAb\nLDSbKBODf9+G33bcycZvqtEDeSSkHAMcEAbVPI9XrfMxCyfSSKXuOcSTKBSNG2Hy\nhiYrNEK/r6+vvL6uROrI0AlbbZjKSxOAgxkz8yBaFQKBgQD1W7ErVNkmz94hawrU\neieGpYf9BjeafbUNf8XHIjaRJDJYdMGTOPHnHRgoR5aiL/afqJt7RM1YaFblUbNk\nktplW5j7V3TVkN/FBgck9RzljtngbMKPUoe2Bpuh+PfMWMFSBrARgBeuDfVC7BZh\nnJ8e4RBFXent/+IYLW23whAo8wKBgQDQ4wJQItSQzxUWk4hx4sDZ1dW4fYWUW5nE\nQQ5R6T6kfjAyl2hXkjHFqEzjsnaAjZxmPjr/4khR+Xm860o6xOacfUbLKhPVEMtp\n3fSk2hKshBvCKUffvK4kb5ZLv6J5k3BHccnT4h7mvMpvfD32VVPHqyRX5simg/3q\nmUj5OUvknwKBgQCtGkSz2ofd7xe7ogahA5GxWEXKCMHf+EJtRLEnnga7fbsbVzxe\n/vUD5i3t9RvcT1SDLfSMEm2BqxNrdKnJEiaNDgOvh/NA3ZZSLb+KnngFqTQkNvdJ\nIKPok17n9nv1KsUxdtBveFy8itJ3pZLgyRwmMAlHt0tcg4RjJGHdHEhEawKBgDO/\nyZboyhiF9Qd+zVLJhlVxMF1gW3OBrfp803tfeXFvAanG8MahJUt+gm32jH0UmCaM\nTDmQabJFR++RqvYcSbWDI8K5Teh/HKXBoxYkIHQ01luntnKrX8kPDAEAHNRZvns0\nqXegkV9AbjZoZUFdqj/RGlPcjOET2hgAmZ+EnlSpAoGAKqNDsIxWLs0xVVRiNLKQ\nh4ahcpGxnOf7XxrxvZB76dn5OqhVP6k3ge45moqosqpwOWTk54UZFWqO+EPWU49H\ncwIq3keemMNwJ+Gry7gCOWJCv3vb1sScEtv/jllftYHmjW3Qoawe3bj16NzAxiLL\nZP5IRdwsMyvNJCfLxuQInGI=\n-----END PRIVATE KEY-----\n",
-      "client_email":
-          "firebase-adminsdk-fbsvc@farmers-market-c591f.iam.gserviceaccount.com",
-      "client_id": "112615187189922028403",
-      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-      "token_uri": "https://oauth2.googleapis.com/token",
-      "auth_provider_x509_cert_url":
-          "https://www.googleapis.com/oauth2/v1/certs",
-      "client_x509_cert_url":
-          "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40farmers-market-c591f.iam.gserviceaccount.com",
-      "universe_domain": "googleapis.com"
-    });
-    List<String> scopes = [
-      'https://www.googleapis.com/auth/firebase.messaging'
-    ];
+    try {
+      final serviceAccountJson =
+          await rootBundle.loadString('assets/service_account.json');
 
-    AuthClient client =
-        await clientViaServiceAccount(accountCredentials, scopes);
+      final accountCredentials =
+          ServiceAccountCredentials.fromJson(json.decode(serviceAccountJson));
 
-    return client; // Remember to close the client when you are finished with it.
+      List<String> scopes = [
+        'https://www.googleapis.com/auth/firebase.messaging'
+      ];
+
+      AuthClient client =
+          await clientViaServiceAccount(accountCredentials, scopes);
+
+      return client; // Remember to close the client when you are finished with it.
+    } catch (e) {
+      _exceptionHandler.handleException(e);
+      return null;
+    }
   }
 }
